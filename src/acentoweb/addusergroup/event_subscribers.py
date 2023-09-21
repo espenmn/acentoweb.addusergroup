7cd9d1fb5d41678b6fc2793921cc846616356fd8
Espen Moe-Nilssen <espen@medialog.no>
2023-09-21 15:48:21 +0200

    Update event_subscribers.py

--- src/acentoweb/addusergroup/event_subscribers.py
+++ src/acentoweb/addusergroup/event_subscribers.py
@@ -6,11 +6,7 @@ from acentoweb.addusergroup.interfaces import IUserGroupSettings
 def add_user_to_group(event):
     """Event subscriber to add a user to specific groups after the first login."""
 
-    # Get the user object from the event
-    user = event.object
-    
     # Get the user id
-    #user_id = user.getId()
     user_id =  api.user.get_current()
 
     groups =   api.portal.get_registry_record('usergroup', interface=IUserGroupSettings)
