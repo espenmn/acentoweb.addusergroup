3617988332e8f98d2e04e49ad6390f2de9a44f58
Espen Moe-Nilssen <espen@medialog.no>
2023-09-22 11:55:57 +0200

    Change adding groups to 'just first login'

--- src/acentoweb/addusergroup/event_subscribers.py
+++ src/acentoweb/addusergroup/event_subscribers.py
@@ -13,8 +13,11 @@ def add_user_to_group(event):
 
     # Add the user to the groups
     if groups:
+        #if user_id.getProperty('login_time').year() < 2020:
+        # user_id.getGroups()
         for group in groups:
-            api.group.add_user(groupname=group, user=user_id)
+            if not group in user_id.getGroups():
+                api.group.add_user(groupname=group, user=user_id)
 
     return True
             
